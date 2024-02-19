f0ffbff9610530ed2a29b123c44546dc1cddb8a8
Rumen Botev <rumbata2409@gmail.com>
2024-02-19 18:20:44 +0200

    90% done

--- index.js
+++ index.js
@@ -1,82 +1,196 @@
-//  542bec5bdbf6414fa69ee266a887429f- API Key
-// https://api.spoonacular.com/recipes/findByIngredients - fetch url
+// Onload main course displayed items
+const dataStatsTemplate = document.querySelector("[data-stats-template]");
+const statsContainer = document.querySelector("[data-stats-container]");
 
-// const inputValue = searchInput.value;
-// const ingredients = inputValue.split(",").map((item) => item.trim()); // Split the input value by commas and trim whitespace
-// const recipes = await findRecipesByIngredients(ingredients);
+const dataIngredientContainer = document.querySelector(
+  "[data-ingredient-container]"
+);
+const dataIngredientsTemplate = document.querySelector(
+  "[data-ingredients-template]"
+);
 
-// Fetching Data from API
+const ingredientsArr = [
+  {
+    ingredients: "500g mixed vegetables",
+  },
+  {
+    ingredients: "2 garlic cloves",
+  },
+  {
+    ingredients: "1 tsp thyme leaves or chopped rosemary",
+  },
+  {
+    ingredients: "100g Cropwell Bishop Stilton, crumbled",
+  },
+  {
+    ingredients: "2 tbsp cream or crème fraîche",
+  },
+  {
+    ingredients: "1 tsp Dijon mustard",
+  },
+  {
+    ingredients: "pinch of ground cayenne or paprika",
+  },
+  {
+    ingredients: "small squeeze lemon juice",
+  },
+  {
+    ingredients: "2 fillet steaks",
+  },
+  {
+    ingredients: "oil for frying, e.g. vegetable or sunflower",
+  },
+  {
+    ingredients: "salt and pepper",
+  },
+];
 
-let users = [];
+const loadCookingSteps = () => {
+  const statsElement = dataStatsTemplate.content.cloneNode(true);
 
+  statsContainer.appendChild(statsElement);
+
+  ingredientsArr.forEach((ingredient) => {
+    const ingredientElement = dataIngredientsTemplate.content.cloneNode(true);
+
+    const dataCheckbox = ingredientElement.querySelector("[data-checkbox]");
+
+    dataCheckbox.innerText = ingredient.ingredients;
+    dataIngredientContainer.appendChild(ingredientElement);
+  });
+};
+
+window.onload = loadCookingSteps;
+
+// Displaying data dynamically from API
 const userRecipeTemplate = document.querySelector("[data-user-template]");
 const dataUserTemplateContainer = document.querySelector(
   "[data-user-recipe-container]"
 );
-
 const searchInput = document.querySelector("[data-search]");
-
-searchInput.addEventListener("input", (e) => {
-  const value = e.target.value.toLowerCase();
-  findRecipesByIngredients([value]);
-  users.forEach((user) => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.image.toLowerCase().includes(value);
-    user.element.classList.toggle("hide", !isVisible);
-    console.log(isVisible);
-  });
-});
-
 const recipeTitle = document.querySelector(".recipeTitle");
+const course = document.querySelector(".course");
+const displayRecipeImg = document.querySelector(".change_img");
+const active = document.querySelector(".active");
+const recipe_uri = document.querySelector(".recipe_uri");
 
-dataUserTemplateContainer.addEventListener("click", (e) => {
-  if (e.target.closest(".autocomplete-container")) {
-    const recipeName = e.target
-      .closest(".autocomplete-container")
-      .querySelector("[data-header]").textContent;
-    recipeTitle.innerText = recipeName;
-    searchInput.value = ''
-    
-   
-    
-  }
-});
+// Function to update the displayed recipes
+function updateDisplayedRecipes(recipes) {
+  dataUserTemplateContainer.innerHTML = ""; // Clear previous suggestions
 
-async function findRecipesByIngredients(ingredients) {
-  const API_KEY = `542bec5bdbf6414fa69ee266a887429f`;
-  const API_URL = `https://api.spoonacular.com/recipes/findByIngredients`;
+  recipes.forEach((recipe) => {
+    const recipeElement = userRecipeTemplate.content.cloneNode(true);
 
-  // Construct the query parameters
-  const queryParams = new URLSearchParams({
-    ingredients: ingredients.join(","),
-    number: 4, // Example number of recipes you want to retrieve
-    apiKey: API_KEY,
+    const recipeHeader = recipeElement.querySelector("[data-header]");
+    const recipeImg = recipeElement.querySelector("[data-img]");
+
+    recipeHeader.textContent = recipe.recipe.label;
+    recipeImg.src = recipe.recipe.image;
+    active.style.display = "block";
+
+    recipeElement
+      .querySelector(".autocomplete-container")
+      .addEventListener("click", (e) => {
+        recipeTitle.innerText = recipe.recipe.label;
+        searchInput.value = "";
+        displayRecipeImg.src = recipe.recipe.image;
+        active.style.display = "none";
+        course.textContent = `${recipe.recipe.dishType || "N/A"}`;
+        recipe_uri.href = recipe.recipe.url;
+        console.log(recipe.recipe.url);
+        console.log(recipe);
+
+        dataIngredientContainer.innerHTML = "";
+        recipe.recipe.ingredientLines.forEach((ingredient) => {
+          const ingredientElement =
+            dataIngredientsTemplate.content.cloneNode(true);
+
+          const dataCheckbox =
+            ingredientElement.querySelector("[data-checkbox]");
+
+          dataCheckbox.innerText = ingredient;
+          dataIngredientContainer.appendChild(ingredientElement);
+        });
+
+        // Update ingredients
+        // Clear the existing content of statsContainer
+        statsContainer.innerHTML = "";
+
+        // Clone the template content
+
+        const statsElement = dataStatsTemplate.content.cloneNode(true);
+        // Update template with recipe data
+
+        statsElement.querySelector("[data-calorie]").innerText = `${Math.round(
+          recipe.recipe.calories
+        )} calories`;
+
+        const dataAttributes = [
+          {
+            key: "data-meal-type",
+            label: "Meal type",
+            value: recipe.recipe.mealType || "N/A",
+          },
+          {
+            key: "data-cuisineType",
+            label: "Cuisine Type",
+            value: recipe.recipe.cuisineType || "N/A",
+          },
+          {
+            key: "data-healthLabels",
+            label: "Health Labels",
+            value: recipe.recipe.healthLabels.join(", ") || "N/A",
+          },
+          {
+            key: "data-dietLabels",
+            label: "Diet Labels",
+            value: recipe.recipe.dietLabels.join(", ") || "N/A",
+          },
+        ];
+
+        dataAttributes.forEach((attribute) => {
+          const element = statsElement.querySelector(`[${attribute.key}]`);
+          element.querySelector("strong").innerText = `${attribute.label}:`;
+          element.querySelector("span").innerText = attribute.value;
+        });
+
+        statsContainer.appendChild(statsElement);
+      });
+
+    dataUserTemplateContainer.appendChild(recipeElement);
   });
 
-  const urlWithParams = `${API_URL}?${queryParams}`;
+  if (recipes.length === 0) {
+    const noResultsElement = document.createElement("div");
+    noResultsElement.textContent = "No recipes found";
+    dataUserTemplateContainer.appendChild(noResultsElement);
+  }
+}
 
-  // Example --> https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2
+// Event listener for the check the recipe here
+
+// Event listener for input on search
+searchInput.addEventListener("input", async (e) => {
+  const value = e.target.value.toLowerCase().trim();
+  const recipes = await findRecipesByQuery(value);
+  updateDisplayedRecipes(recipes);
+});
+
+async function findRecipesByQuery(query) {
+  const API_ID = "e609571a";
+  const API_KEY = "17e642e5eaa6371efff804c1e4dc142a	";
+  const API_URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${API_ID}&app_key=${API_KEY}&random=true&limit=4`;
 
   try {
-    const response = await fetch(urlWithParams);
+    const response = await fetch(API_URL);
     if (response.ok) {
       const data = await response.json();
-      users = data.map((user) => {
-        const recipe = userRecipeTemplate.content.cloneNode(true).children[0];
-        const recipe_header = recipe.querySelector("[data-header]");
-        const recipeImg = recipe.querySelector("[data-img]");
-        recipe_header.textContent = user.title;
-        recipeImg.src = user.image;
-        dataUserTemplateContainer.append(recipe);
-        return { name: user.name, image: user.image, element: recipe };
-      });
-      // return data;
+      return data.hits;
     } else {
       throw new Error(`Error fetching data: ${response.statusText}`);
     }
   } catch (err) {
     console.error(err);
-    return null;
+    return [];
   }
 }
